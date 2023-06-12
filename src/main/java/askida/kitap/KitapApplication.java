2bc3281bbf8c1e403fc8711bcf8129d17bdf4104
brylmaz <brylmaz52@gmail.com>
2023-06-13 00:30:48 +0300

    create spring template

---
+++ src/main/java/askida/kitap/KitapApplication.java
@@ -0,0 +1,13 @@
+package askida.kitap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/askida/kitap/KitapApplicationTests.java
@@ -0,0 +1,13 @@
+package askida.kitap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
